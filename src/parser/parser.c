a9f43528450603bcd87a78dcee03e16145f541fa
alecmalloc <aeastman@student.42berlin.de>
2023-11-03 16:26:56 +0100

    fixed not compiling, work in progress on command list init

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,29 +6,50 @@
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:20:08 by aeastman          #+#    #+#             */
-/*   Updated: 2023/11/03 16:04:18 by aeastman         ###   ########.fr       */
+/*   Updated: 2023/11/03 16:26:31 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-t_list *new_node(t_shell *shell)
+t_clist *new_node(t_shell *shell)
 {
-	
+	t_clist *clist_node;
+
+	clist_node = (t_clist *)malloc(sizeof(t_clist));
+	clist_node->next = NULL;
+
+	return (clist_node);
 }
 
-t_clist *clist_init(t_shell *shell)
+void insert_node(t_shell *shell, t_clist *node)
+{
+	t_clist **tracer;
+
+	tracer = &shell->clist;
+	while (*tracer)
+		tracer = &(*tracer)->next;
+	node->next = *tracer;
+	*tracer = node;
+}
+
+void clist_init(t_shell *shell)
 {
 	int n_nodes;
-	t_clist clist_head;
+	t_clist *node;
 
-	n_nodes = shell->n_pipes + 1;
+	n_nodes = (shell->n_pipes + 1) + 1;
 
+	while (--n_nodes)
+	{
+		node = new_node(shell);
+		insert_node(shell, node);
+	}
 
-	return (clist_head);
 }
 
 int	parser(t_shell *shell)
 {
-	shell->clist = clist_init(shell);
+	shell->clist = NULL;
+	clist_init(shell);
 }
