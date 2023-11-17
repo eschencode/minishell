b89998b9f15c7e8eeaf9430da4f6a963ca19e883
alecmalloc <aeastman@student.42berlin.de>
2023-11-17 10:59:02 +0100

    cleaned up some stuff

--- src/builtins/builtins_a.c
+++ src/builtins/builtins_a.c
@@ -6,42 +6,57 @@
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 12:41:06 by aeastman          #+#    #+#             */
-/*   Updated: 2023/11/16 17:46:17 by aeastman         ###   ########.fr       */
+/*   Updated: 2023/11/17 10:58:43 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-bool clearwindow(void)
+bool	clearwindow(void)
 {
 	printf("\033[H\033[J");
 	return (true);
 }
 
-int count_str_arr(char **str)
+int	count_str_arr(char **str)
 {
-	int y;
+	int	y;
+
 	y = 0;
 	while (str[y])
 		y++;
 	return (y);
 }
 
-void str_arr_cpy(char **new, t_shell *shell)
+void	str_arr_cpy(char **new, char **old)
 {
-	int y;
+	int	y;
+
 	y = -1;
-	while (shell->env[++y])
+	while (old[++y])
 	{
-		new[y] = malloc(sizeof(char) * (ft_strlen(shell->env[y]) + 1));
-		ft_strlcpy(new[y], shell->env[y], ft_strlen(shell->env[y]));
+		new[y] = malloc(sizeof(char) * (ft_strlen(old[y]) + 1));
+		ft_strlcpy(new[y], old[y], ft_strlen(old[y]));
 	}
 }
 
-bool ft_export(t_shell *shell, char **cmd)
+void	free_env(t_shell *shell)
 {
-	char **env;
-	int old_len;
+	int	y;
+
+	y = -1;
+	if (shell->env_flag)
+	{
+		while (shell->env[++y])
+			free(shell->env[y]);
+		free(shell->env);
+	}
+}
+
+bool	ft_export(t_shell *shell, char **cmd)
+{
+	char	**env;
+	int		old_len;
 
 	old_len = 0;
 	if (shell->env_flag == 1)
@@ -50,13 +65,13 @@ bool ft_export(t_shell *shell, char **cmd)
 	if (shell->env_flag == 0)
 	{
 		env[0] = malloc(sizeof(char) * (ft_strlen(cmd[1]) + 1));
-	    ft_strlcpy(env[0], cmd[1], ft_strlen(cmd[1]));
+		ft_strlcpy(env[0], cmd[1], ft_strlen(cmd[1]));
 		env[1] = NULL;
 		shell->env = env;
 		shell->env_flag = 1;
 		return (true);
 	}
-	str_arr_cpy(env, shell);
+	str_arr_cpy(env, shell->env);
 	free(shell->env);
 	env[old_len] = malloc(sizeof(char) * (ft_strlen(cmd[1]) + 1));
 	ft_strlcpy(env[old_len], cmd[1], ft_strlen(cmd[1]));
