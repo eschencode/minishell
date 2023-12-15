7b075be8927c4bab118b5bc491984d988a08c90a
alecmalloc <aeastman@student.42berlin.de>
2023-12-15 14:08:16 +0100

    working heredoc prototype pre memory leak check

--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins_a_3.c                                     :+:      :+:    :+:   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 15:41:20 by aeastman          #+#    #+#             */
-/*   Updated: 2023/12/15 12:56:20 by aeastman         ###   ########.fr       */
+/*   Updated: 2023/12/15 14:07:24 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,53 @@ int heredoc_syntax_check(t_clist *cmd)
 		printf("msh: parse error near newline\n");
 		return (1);
 	}
-	
-	
 	return (0);
+}
+
+int		heredoc_create(void)
+{
+	int fd;
+	fd = open("heredoc.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	return (fd);
+}
+
+char	*get_heredoc_key(t_clist *cmd)
+{
+	char *key;
+	char *new_str;
+
+	key = cmd->cmd[1];
+	key = key + 2;
+	new_str = ft_strdup(key);
+	return (new_str);
 }
 
 bool	ft_heredoc(t_shell *shell, t_clist *cmd)
 {
+	int input_ret;
+	int heredoc_fd;
+	char *heredoc_key;
+	char *heredoc_input;
+	
 	if (heredoc_syntax_check(cmd) == 1)
 		return (true);
-	return (true);
+	heredoc_fd = heredoc_create();
+	if (heredoc_fd == -1)
+		return (true);
+	heredoc_key = get_heredoc_key(cmd);
+	heredoc_input = NULL;
+	input_ret = -1;
+	while (input_ret != 0)
+	{
+		heredoc_input = readline("heredoc> ");
+		input_ret = ft_strncmp(heredoc_key, heredoc_input, ft_strlen(heredoc_input));
+		if (input_ret != 0)
+		{
+			write(heredoc_fd, heredoc_input, ft_strlen(heredoc_input));
+			write(heredoc_fd, "\n", 1);
+		}
+	}
+	ft_strlcpy(cmd->cmd[1], "heredoc.txt", 11);
+
+	return (false);
 }
