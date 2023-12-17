f89e02c1f1708b4971a622c34b55db49855ba299
alecmalloc <aeastman@student.42berlin.de>
2023-12-17 13:21:04 +0100

    INDEX out of whack LOL need to subtract one silly goose

--- src/expander/expander_quotes.c
+++ src/expander/expander_quotes.c
@@ -6,7 +6,7 @@
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 13:57:14 by aeastman          #+#    #+#             */
-/*   Updated: 2023/12/17 12:31:58 by aeastman         ###   ########.fr       */
+/*   Updated: 2023/12/17 13:19:36 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char *ft_realloc(char *str, int size)
 
 	old_size = ft_strlen(str);
 	new_str = malloc(sizeof(char) * (old_size + size + 1));
-	strcpy(new_str, str);
+	strncpy(new_str, str, old_size);
 	free(str);
 	return (new_str);
 }
@@ -29,9 +29,10 @@ void shift_tokens_up(t_shell *shell, int index, int size) {
 	int i;
 
 	i = index;
+	printf("freeing at index->%d\n", index);
 	free(shell->tokens[index].token);
 
-    while (i < size - 1) {
+    while (i < size) {
         shell->tokens[i].token = shell->tokens[i + 1].token;
 		i++;
     }
@@ -46,6 +47,7 @@ int		get_tokens_len(t_shell *shell)
 	i = 0;
 	while (shell->tokens[i].token)
 		i++;
+	printf("arr len -> %d\n", i);
 	return (i);
 }
 
@@ -63,25 +65,19 @@ void	expander_quotes(t_shell *shell)
 	while (shell->tokens[++i].token)
 	{
 		token_str = shell->tokens[i].token;
+		printf("reading chr->%s\n", token_str);
 		if (first_flag == 0 && ft_strchr(token_str, '\"') != NULL)
 		{
 			first_string = token_str;
-			new_str = ft_strdup(first_string);
 			shell->tokens[i].type = WORD;
 			first_flag = 1;
 		}
 		else if (first_flag == 1)
 		{
-			new_str = ft_realloc(new_str, ft_strlen(token_str));
-			strcat(new_str, " ");
-			strcat(new_str, token_str);
-			printf("new->%s\n", new_str);
 			if (ft_strchr(token_str, '\"') != NULL)
 				first_flag = 0;
 			shift_tokens_up(shell, i, get_tokens_len(shell));
-			strcpy(first_string, new_str);
+			i--;
 		}
 	}
 }
-
-
