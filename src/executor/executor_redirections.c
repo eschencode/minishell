b8bdd815f2759a7c9da104b1baf4550936bc6e29
alecmalloc <aeastman@student.42berlin.de>
2024-01-09 10:07:14 +0000

    fixed cd - memory leaks in check_for_redirections_out

--- src/executor/executor_redirections.c
+++ src/executor/executor_redirections.c
@@ -6,7 +6,7 @@
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 11:41:15 by leschenb          #+#    #+#             */
-/*   Updated: 2024/01/08 15:29:44 by aeastman         ###   ########.fr       */
+/*   Updated: 2024/01/09 10:06:56 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,27 +23,27 @@ bool check_for_redirections_out(t_shell *shell, t_clist *cmd)
 	int i;
 
 	i = 0;
-	if (shell->num_tokens <= 1)
+	
+	// added for memory safety
+	if (strstr(shell->input_str, ">>") == NULL && strstr(shell->input_str, ">") == NULL && strstr(shell->input_str, "<") == NULL)
 		return (false);
 
 	while(shell->tokens[i].token && (strcmp(shell->tokens[i].token, cmd->cmd[0]) != 0))
 		i++;
 	i++;
-	if(shell->tokens[i].token[0] == '-')//skips options
+	if(shell->tokens[i].token[0] == '-') //skips options
 		i++;
-
-	printf("%d\n", shell->tokens[i].type);
 	if (shell->tokens[i].type == RIGHT)
 		shell->redirect_out = open(shell->tokens[i + 1].token, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	else if(shell->num_tokens > 2 && shell->tokens[i + 2].type == RIGHT)
+	else if(shell->tokens[i + 2].type == RIGHT)
 		shell->redirect_out = open(shell->tokens[i + 3].token, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if(shell->tokens[i].type == RIGHT_RIGHT)
 		shell->redirect_out = open(shell->tokens[i + 1].token,  O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-	else if( shell->num_tokens > 2 && shell->tokens[i + 2].type == RIGHT_RIGHT)
+	else if(shell->tokens[i + 2].type == RIGHT_RIGHT)
 		shell->redirect_out = open(shell->tokens[i + 3].token,  O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
 	if(shell->tokens[i].type == LEFT)//add check if out and in check for more tokens
 		shell->redirect_in = open(shell->tokens[i + 1].token, O_RDONLY);
-	else if(shell->num_tokens > 2 && shell->tokens[i + 2].type == LEFT)//add check if out and in check for more tokens
+	else if(shell->tokens[i + 2].type == LEFT) //add check if out and in check for more tokens
 		shell->redirect_in = open(shell->tokens[i + 3].token, O_RDONLY);
 	return(false);
 }
