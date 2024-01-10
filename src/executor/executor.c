2ec5ee8d2a0b379e629d0377ed23d921992a242e
alecmalloc <aeastman@student.42berlin.de>
2024-01-10 12:09:56 +0000

    fixed issue

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 14:52:52 by aeastman          #+#    #+#             */
-/*   Updated: 2024/01/10 12:02:32 by aeastman         ###   ########.fr       */
+/*   Updated: 2024/01/10 12:09:36 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int execute_externals(t_shell *shell)
 	}
 	else if (child_pid == 0)
 	{// This code is executed in the child process
-		if (shell->path != NULL && execve(shell->path, shell->clist->cmd, shell->env) == -1) //shell->env;
+		if (execve(shell->path, shell->clist->cmd, shell->env) == -1) //shell->env;
 		{
 			printf("command not found: %s\n",shell->clist->cmd[0]);
 			ft_error("exec error");
@@ -109,7 +109,8 @@ int	executor(t_shell *shell)
 	if(check_if_builtin(shell, *cmd, 0, 1) == false)
 	{
 		shell->path = exe_path(shell, shell->clist->cmd[0]);
-		execute_externals(shell);
+		if (shell->path != NULL)
+			execute_externals(shell);
 	}
 	restore_stdin_stdout(saved_stdin, saved_stdout);
 	return (0);
