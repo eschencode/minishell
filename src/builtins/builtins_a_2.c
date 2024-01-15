0831ccc219cd2618839a535e6873925c0a92661e
alecmalloc <aeastman@student.42berlin.de>
2024-01-15 10:55:40 +0000

    seperated echo into its own file

--- src/builtins/builtins_a_2.c
+++ src/builtins/builtins_a_2.c
@@ -6,7 +6,7 @@
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 11:07:36 by aeastman          #+#    #+#             */
-/*   Updated: 2024/01/15 10:26:48 by aeastman         ###   ########.fr       */
+/*   Updated: 2024/01/15 10:55:12 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,70 +63,6 @@ bool ft_unset(t_shell *shell, char *cmd, int fd_in, int fd_out)
     shell->env[y] = NULL;
 
     return true;
-}
-
-int     validate_n_flag(char *str)
-{
-    int x;
-
-    x = 0;
-    while (str[++x] && str[x] != ' ' && str[x] != '\t')
-    {
-        if (str[x] != 'n')
-            return (0);
-    }
-    return (1);
-}
-
-bool    ft_echo(t_shell *shell, t_clist *c_node, int fd_in, int fd_out)
-{
-    int y;
-    int n;
-    int n_flag;
-    int word_flag;
-    int dont_print;
-    char *var;
-    char *n_str;
-    char **cmd;
-
-    ft_dup2(fd_in, fd_out);
-    y = 0;
-    n_flag = 0;
-    word_flag = 0;
-    n = c_node->n_args;
-    cmd = c_node->cmd;
-    while (cmd[++y])
-    {
-        dont_print = 0;
-        if ((n_str = strstr(cmd[y], "-n")) != NULL)
-        {
-            if (validate_n_flag(n_str) == 1)
-            {
-                n_flag = 1;
-                if (word_flag == 0)
-                    dont_print = 1;
-            }
-        }
-        if ((var = strstr(cmd[y], "$")) != NULL)
-        {
-            if (env_get_val(shell, var) != NULL)
-                printf("%s", cmd[y]);
-        }
-        else
-        {
-            if (dont_print == 0)
-            {
-                printf("%s", cmd[y]);
-                word_flag = 1;
-                if (n != 1)
-                    printf(" ");
-            }
-        }
-        n--;
-    }
-    if (n_flag == 0)
-        printf("\n");
-    return (true);
 }
 
 char *env_get_val(t_shell *shell, char *var)
