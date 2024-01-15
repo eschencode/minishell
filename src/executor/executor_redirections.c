9571cfb6fec7f820234cca04df3ecead2e10f497
Linus Eschenbach <leschenb@c4a10c8.42berlin.de>
2024-01-15 16:22:26 +0100

    better redir_check + fixed memory issue

--- src/executor/executor_redirections.c
+++ src/executor/executor_redirections.c
@@ -17,35 +17,36 @@
 >: Redirects standard output to a file, overwriting its contents if the file exists
 >>: Appends standard output to a file, preserving existing content and adding new output at the end of the file.
 <: Redirects standard input to come from a file rather than from the keyboard*/
-
-bool check_for_redirections_out(t_shell *shell, t_clist *cmd)
+bool	check_redirections(t_shell *shell,t_clist *cmd,int *fd_in, int *fd_out)
 {
-	int i;
-
+	int	i;
 	i = 0;
-	
-	// added for memory safety
 	if (strstr(shell->input_str, ">>") == NULL && strstr(shell->input_str, ">") == NULL && strstr(shell->input_str, "<") == NULL)
 		return (false);
-
 	while(shell->tokens[i].token && (strcmp(shell->tokens[i].token, cmd->cmd[0]) != 0))
 		i++;
 	i++;
+	if (shell->tokens[i].token == NULL)
+    	return false;
 	if(shell->tokens[i].token[0] == '-') //skips options
 		i++;
-	while(shell->tokens[i].token && shell->tokens[i].type == ARG)
-		i++;
-	if (shell->tokens[i].type == RIGHT)
-		shell->redirect_out = open(shell->tokens[i + 1].token, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	else if(shell->tokens[i + 2].type == RIGHT)
-		shell->redirect_out = open(shell->tokens[i + 3].token, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if(shell->tokens[i].type == RIGHT_RIGHT)
-		shell->redirect_out = open(shell->tokens[i + 1].token,  O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-	else if(shell->tokens[i + 2].type == RIGHT_RIGHT)
-		shell->redirect_out = open(shell->tokens[i + 3].token,  O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-	if(shell->tokens[i].type == LEFT)//add check if out and in check for more tokens
-		shell->redirect_in = open(shell->tokens[i + 1].token, O_RDONLY);
-	else if(shell->tokens[i + 2].type == LEFT) //add check if out and in check for more tokens
-		shell->redirect_in = open(shell->tokens[i + 3].token, O_RDONLY);
-	return(false);
+	if (shell->tokens[i].token == NULL)
+		return false;
+	while(shell->tokens[i].token && shell->tokens[i].type == ARG)//skip args
+		      i++;
+	if (i + 1 < shell->num_tokens && shell->tokens[i+1].token && shell->tokens[i].type == RIGHT)
+		*fd_out = open(shell->tokens[i + 1].token, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	else if (i + 3 < shell->num_tokens && shell->tokens[i +3].token && shell->tokens[i + 2].type == RIGHT)
+		*fd_out  = open(shell->tokens[i + 3].token, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if(i + 1 < shell->num_tokens && shell->tokens[i+1].token && shell->tokens[i].type == LEFT)
+		*fd_in = open(shell->tokens[i + 1].token, O_RDONLY);
+	else if (i + 3 < shell->num_tokens && shell->tokens[i + 3].token && shell->tokens[i + 2].type == LEFT)
+		*fd_in = open(shell->tokens[i + 3].token, O_RDONLY);
+	if(i + 1 < shell->num_tokens && shell->tokens[i + 1].token && shell->tokens[i].type == RIGHT_RIGHT)
+		*fd_out = open(shell->tokens[i + 1].token, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	else if(i + 3 < shell->num_tokens && shell->tokens[i + 3].token && shell->tokens[i + 2].type == RIGHT_RIGHT)
+		*fd_out = open(shell->tokens[i + 3].token, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	if(*fd_out == -1 || *fd_in == -1)
+		printf("error accesing file\n");//add more eroro here probaby clos the fd too :()
+	return(0);
 }
