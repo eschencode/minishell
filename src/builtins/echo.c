b3ef990a8fc0ee1fc80ef4ce205d7d66a8cf4bb3
alecmalloc <aeastman@student.42berlin.de>
2024-01-21 14:30:11 +0100

    better echo prototype

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,25 +6,49 @@
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 10:54:02 by aeastman          #+#    #+#             */
-/*   Updated: 2024/01/21 14:03:53 by aeastman         ###   ########.fr       */
+/*   Updated: 2024/01/21 14:29:09 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+int     echo_match_flag(char *str, int *nl_flag)
+{
+    int x;
+
+    x = 0;
+    if (str[0] != '-')
+        return (0);
+    while (str[++x])
+    {
+        if (str[x] != 'n')
+            return (0);
+    }
+    *nl_flag = 1;
+    return (1); 
+}
+
 bool    ft_echo(t_shell *shell, t_clist *c_node, int fd_in, int fd_out)
 {
     int y;
+    int nl_flag;
+    int printed;
     char *arg;
     ft_dup2(fd_in, fd_out);
 
     y = 0;
+    printed = 0;
+    nl_flag = 0;
     while ((arg = c_node->cmd[++y]) != NULL)
     {
-        if (y > 1)
-            printf(" ");
-        printf("%s", arg);
+        if (echo_match_flag(arg, &nl_flag) != 1)
+        {
+            if (y > 1 && printed > 0)
+                printf(" ");
+            printed += printf("%s", arg);
+        }
     }
-    printf("\n");
+    if (nl_flag != 1)
+        printf("\n");
     return (true);
 }
