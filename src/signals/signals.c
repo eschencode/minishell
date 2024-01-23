44e9a6b665234c238ac9e389cf0e29fb6ad561a4
Linus Eschenbach <leschenb@c4a10c8.42berlin.de>
2024-01-23 16:49:57 +0100

    norm signals

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leschenb <leschenb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 16:45:42 by leschenb          #+#    #+#             */
+/*   Updated: 2024/01/23 16:48:53 by leschenb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/minishell.h"
 
-void sigint_handler(int sig)
+void	sigint_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
@@ -11,29 +23,23 @@ void sigint_handler(int sig)
 	}
 }
 
-void sigquit_handler(int sig)
+void	sigquit_handler(int sig)
 {
-
 	if (sig == SIGQUIT)
 	{
-		 // Move the cursor to the beginning of the line
-        write(1, "\r", 1);
-        // Clear the line
-        write(1, "\033[K", 3);
-        // Reprint the line
-        printf("msh$ %s", rl_line_buffer);
-        // Move readline's cursor to the end of the line
-        rl_point = rl_end;
+		write(1, "\r", 1);
+		write(1, "\033[K", 3);
+		printf("msh$ %s", rl_line_buffer);
+		rl_point = rl_end;
 	}
 }
 
-void sigs_init(struct sigaction *sa_int, struct sigaction *sa_quit)
+void	sigs_init(struct sigaction *sa_int, struct sigaction *sa_quit)
 {
 	sa_int->sa_handler = sigint_handler;
 	sigemptyset(&sa_int->sa_mask);
 	sa_int->sa_flags = 0;
 	sigaction(SIGINT, sa_int, NULL);
-
 	sa_quit->sa_handler = sigquit_handler;
 	sigemptyset(&sa_quit->sa_mask);
 	sa_quit->sa_flags = 0;
