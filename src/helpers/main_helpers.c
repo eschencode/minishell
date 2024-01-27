16ea6b1e68991d511cbab3b60b16be65f98cd97a
alecmalloc <aeastman@student.42berlin.de>
2024-01-27 13:22:29 +0100

    removed infinite loop on > file or >> file

--- src/helpers/main_helpers.c
+++ src/helpers/main_helpers.c
@@ -6,7 +6,7 @@
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 19:05:57 by aeastman          #+#    #+#             */
-/*   Updated: 2024/01/27 13:17:35 by aeastman         ###   ########.fr       */
+/*   Updated: 2024/01/27 13:21:49 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,8 @@ int	weird_token_catcher(t_shell *shell)
 
 	x = 0;
 	str = shell->input_str;
+	if (str[0] == '>' || str[1] == '>')
+		return (1);
 	while (str[x] && (str[x] == '<' || str[x] == '>' || str[x] == ' ' \
 	|| str[x] == '|' || str[x] == '!'))
 		x++;
